00fa177e5a2804ae02250185469a4dc3dab89150
sonerarslan2021 <metaxdoor@gmail.com>
2022-01-16 18:31:30 +0300

    calisma 1

---
+++ simple_ModbusTCP_server.py
@@ -0,0 +1,29 @@
+#!/bin/python
+"""
+https://github.com/Johannes4Linux/Simple-ModbusTCP-Server
+https://www.youtube.com/watch?v=FYPQgnQE9fk
+"""
+
+from pyModbusTCP.server import ModbusServer, DataBank
+from time import sleep
+from random import uniform
+
+# Create an instance of ModbusServer
+server = ModbusServer("127.0.0.1", 12345, no_block=True)
+
+try:
+    print("Start server...")
+    server.start()
+    print("Server is online")
+    state = [0]
+    while True:
+        DataBank.set_words(0, [int(uniform(0, 100))])
+        if state != DataBank.get_words(1):
+            state = DataBank.get_words(1)
+            print("Value of Register 1 has changed to " +str(state))
+        sleep(0.5)
+
+except:
+    print("Shutdown server ...")
+    server.stop()
+    print("Server is offline")
